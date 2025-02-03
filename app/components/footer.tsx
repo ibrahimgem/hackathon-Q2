1ae742f370c33510f95beea6d6509760220f421a
ibrahim <iby.khan7@gmail.com>
2025-02-03 18:15:39 +0500

    fix

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,10 +2,10 @@ import Link from "next/link"
 
 const Footer = () => {
     return (
-        <footer className="min-h-[30rem] m-auto">
-            <div className="flex flex-col sm:flex-row sm:justify-between">
-                <div className="sm:m-20 flex flex-col gap-4">
-                    <div className="sm:mt-0 mt-10 mx-10 text-blue-600 font-bold text-3xl">
+        <footer className="min-h-[30rem] sm:bg-white bg-[#F6F7F9] bg m-auto">
+            <div className="sm:flex sm:justify-between">
+                <div className="sm:m-20">
+                    <div className="py-5 sm:py-0 mx-10 text-blue-600 font-bold text-3xl">
                         <Link href="/">
                             MORENT
                         </Link>
@@ -16,7 +16,7 @@ const Footer = () => {
 
                 </div>
                 <div className="grid grid-cols-2 sm:flex m-10 sm:m-20 sm:gap-24 gap-x-44 gap-y-10 mr-48">
-                    <ul className="text-gray-400 flex flex-col gap-6 min-w-36">
+                    <ul className="text-gray-400 space-y-6 min-w-36">
                         <li className="text-base sm:text-lg font-bold text-gray-800">
                             About
                         </li>
@@ -33,7 +33,7 @@ const Footer = () => {
                             Business Relation
                         </li>
                     </ul>
-                    <ul className="sm:hidden text-gray-400 flex flex-col gap-6 min-w-36">
+                    <ul className="sm:hidden text-gray-400 space-y-6 min-w-36">
                         <li className="text-base sm:text-lg font-bold text-gray-800">
                             Socials
                         </li>
@@ -50,7 +50,7 @@ const Footer = () => {
                             Facebook
                         </li>
                     </ul>
-                    <ul className="text-gray-400 flex flex-col gap-6 min-w-36">
+                    <ul className="text-gray-400 space-y-6 min-w-36">
                         <li className="text-base sm:text-lg font-bold text-gray-800">
                             Community
                         </li>
@@ -67,7 +67,7 @@ const Footer = () => {
                             Invite a friend
                         </li>
                     </ul>
-                    <ul className="hidden text-gray-400 sm:flex flex-col gap-6 min-w-36">
+                    <ul className="hidden sm:block text-gray-400 space-y-6 min-w-36">
                         <li className="text-base sm:text-lg font-bold text-gray-800">
                             Socials
                         </li>
@@ -86,11 +86,11 @@ const Footer = () => {
                     </ul>
                 </div>
             </div>
-            <div className="flex flex-col sm:flex-row sm:justify-between font-bold text-gray-700 sm:border-t-2 sm:py-10 m-auto sm:mx-28 mx-10">
+            <div className="sm:flex sm:justify-between font-bold text-gray-700 sm:border-t-2 sm:py-10 m-auto sm:mx-28 mx-10">
                 <div className="hidden sm:block">
                     ©2022 MORENT. All rights reserved
                 </div>
-                <div className="flex sm:text-base text-sm gap-20 sm:gap-12">
+                <div className="flex sm:text-base text-xs gap-20 sm:gap-12">
                     <div>
                     Privacy & Policy
                     </div>
